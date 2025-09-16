b5e81387328bbaa0513d4fbe056fabc1d03e2dd5
meteg171212-cloud <meteg171212@icloud.com>
2025-09-16 18:08:35 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import random
+choice = int(input("Choose your own password or make automatic one? press 1 for auto and 0 for own"))
 possible_characters = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-length = int(input("How long would you like yoru passsord to be?"))
-protector = ""
-for i in range(length):
-    protector += random.choice(possible_characters)
-print("your password is", protector)
+if choice == 1:    
+    protector = ""
+    length = int(input("How long would you like your passsord to be?"))
+    for i in range(length):
+        protector += random.choice(possible_characters)
+    print("your password is", protector)
+elif choice == 0:
+    protector = input("Enter your password here")
+    print("your password has been set to", protector)
